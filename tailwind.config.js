879b00861d45ec64b081a2e6b7f77c2a81e55bc4
alifmazli <alifmazli01@gmail.com>
2023-01-18 00:24:30 +0800

    Install Flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js, jsx}", "./components/**/*.{js, jsx}"],
+  content: [
+    "./src/**/*.{js, jsx}",
+    "./components/**/*.{js, jsx}",
+    "./node_modules/flowbite-react/**/*.js",
+    "./pages/**/*.{ts,tsx}",
+    "./public/**/*.html",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
 };
