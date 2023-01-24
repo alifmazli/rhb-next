9f1086d1f96ed6f8871e8a261684b3f0be1a727d
alifmazli <alifmazli01@gmail.com>
2023-01-25 07:30:25 +0800

    Change src path to dynamic

--- src/pages/home/next-deals/[id].js
+++ src/pages/home/next-deals/[id].js
@@ -4,7 +4,6 @@ import Header from "@/components/Home/Header/Header";
 import Link from "next/link";
 import data from "./../../../constants/deals-data.js";
 import Head from "next/head.js";
-import NoSSR from "react-no-ssr";
 
 const NextDealsPage = () => {
   const router = useRouter();
@@ -25,32 +24,30 @@ const NextDealsPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NoSSR>
-        <div className="">
-          <Header title="NextDeals" />
-          <Image
-            src="/images/next-deals-img/gocar.bmp"
-            className="image w-full h-auto shadow-xl rounded-lg"
-            alt={deal[i].description}
-            width={500}
-            height={500}
-            placeholder="blur"
-            blurDataURL="/images/next-deals-img/gocar.bmp"
-          />
-          <h2 className="my-2">{deal[i].brandName}</h2>
-          <h3>{deal[i].description}</h3>
-          <Link href="/verify/sign-in">
-            <button className="my-4 relative w-full inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-              <span className="relative w-full px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Redeem now with RHB Next
-              </span>
-            </button>
-          </Link>
-          <Link className="text-xs" href="#">
-            Terms & Conditions
-          </Link>
-        </div>
-      </NoSSR>
+      <div className="">
+        <Header title="NextDeals" />
+        <Image
+          src={deal[i].images}
+          className="image w-full h-auto shadow-xl rounded-lg"
+          alt={deal[i].description}
+          width={500}
+          height={500}
+          placeholder="blur"
+          blurDataURL={deal[i].images}
+        />
+        <h2 className="my-2">{deal[i].brandName}</h2>
+        <h3>{deal[i].description}</h3>
+        <Link href="/verify/sign-in">
+          <button className="my-4 relative w-full inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
+            <span className="relative w-full px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Redeem now with RHB Next
+            </span>
+          </button>
+        </Link>
+        <Link className="text-xs" href="#">
+          Terms & Conditions
+        </Link>
+      </div>
     </>
   );
 };
